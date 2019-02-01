82b6cdafcd21f133d479efeed6427dc386ebe259
kyruski <jamescboyett@gmail.com>
2019-02-01 12:30:34 -0600

    Exports the mongo db and all needed functions

--- database/index.js
+++ database/index.js
@@ -3,8 +3,6 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.one
 
 let userSchema = mongoose.Schema({
   name: String
@@ -29,11 +27,26 @@ let save = (model) => {
   });
 }
 
-let find = (model, params, callback) => {
-  model.find(params, callback);
+let find = (model, conditions, fieldsToReturn, options, callback) => {
+  model.find(conditions, fieldsToReturn, options, callback);
 }
 
-module.exports = {save: save, 
+module.exports = {db: db,
+  save: save, 
   find: find, 
   User: User, 
   Repo: Repo }
+
+
+  /*News.find({
+    deal_id:deal._id // Search Filters
+},
+['type','date_added'], // Columns to Return NEEDS TO BE OBJ OR STRING, can do 'field1 field2' or
+{
+    skip:0, // Starting Row
+    limit:10, // Ending Row
+    sort:{
+        date_added: -1 //Sort by Date Added DESC
+    }
+},
+callback)*/
