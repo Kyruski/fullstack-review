1677865352404f35c531b1597931d697a16ab446
kyruski <jamescboyett@gmail.com>
2019-02-01 14:17:04 -0600

    Adds untested integration for grabbing repos for a user from github, and parsing the data for return

--- helpers/github.js
+++ helpers/github.js
@@ -1,19 +1,52 @@
 const request = require('request');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
+const formatRepo = (item) => {
+  const repoInfo = {
+    repoName: item.full_name,
+    repoInfo: {
+      author: item.owner.login,
+      author_avatar: item.owner.avatar_url,
+      url: item.html_url,
+      stars: item.stargazers_count,
+      forks: forks_count
+    }
+  };
+  return repoInfo;
+}
+
+const formatResponseData = (data) => {
+  let repos = [];
+  for (let item of data) {
+    let repoObj = formatRepo(item);
+    repos.push(repoObj);
+  }
+  return repos;
+}
+
+let getReposByUsername = (username, callback) => {
   // TODO - Use the request module to request repos for a specific
   // user from the github API
 
   // The options object has been provided to help you out, 
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${username}/repos`,
     headers: {
+      'Accept': 'application/vnd.github.v3+json',
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
+
+  request(options, (err, res, body) => {
+    if (err) {
+      callback(err);
+    } else {
+      const repos = formatResponseData(body);
+      callback(null, repos);
+    }
+  }); //(err, res, body) => {}
 
 }
 
