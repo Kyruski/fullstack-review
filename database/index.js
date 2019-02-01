0a1b96a8c323b409506689fb65df47aea23d5ca3
kyruski <jamescboyett@gmail.com>
2019-02-01 12:21:35 -0600

    Adds the find function and User model

--- database/index.js
+++ database/index.js
@@ -6,7 +6,11 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.one
 
+let userSchema = mongoose.Schema({
+  name: String
+})
 
+let User = mongoose.model('User', userSchema);
 
 let repoSchema = mongoose.Schema({ //need new?
   author: String,
@@ -18,11 +22,18 @@ let repoSchema = mongoose.Schema({ //need new?
 
 let Repo = mongoose.model('Repo', repoSchema); //need new?
 
-let save = (repo) => {
-  repo.save( (err, response) => {
-    if (err) return console.log('err saving Repo', err);
-    console.log('successfully made: ', repo);
+let save = (model) => {
+  model.save(params, (err, response) => {
+    if (err) return console.log('err saving Model', err);
+    console.log('successfully saved model: ', response);
   });
 }
 
-module.exports.save = save;
+let find = (model, params, callback) => {
+  model.find(params, callback);
+}
+
+module.exports = {save: save, 
+  find: find, 
+  User: User, 
+  Repo: Repo }
