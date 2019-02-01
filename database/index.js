be45dc3f5d9a89b2a17d5887106cca755d0aa6bd
kyruski <jamescboyett@gmail.com>
2019-02-01 12:10:32 -0600

    Adds the database schema

--- database/index.js
+++ database/index.js
@@ -1,16 +1,28 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.one
+
+
+
+let repoSchema = mongoose.Schema({ //need new?
+  author: String,
+  author_avatar: String,
+  link: String,
+  stars: Number,
+  forks: Number
 });
 
-let Repo = mongoose.model('Repo', repoSchema);
+let Repo = mongoose.model('Repo', repoSchema); //need new?
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (repo) => {
+  repo.save( (err, response) => {
+    if (err) return console.log('err saving Repo', err);
+    console.log('successfully made: ', repo);
+  });
 }
 
 module.exports.save = save;
