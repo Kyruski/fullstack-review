9a8c6a4fd2fb3249f37d54836d3697510a9c0964
kyruski <jamescboyett@gmail.com>
2019-02-02 12:04:03 -0600

    Updates repos title

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -3,7 +3,7 @@ import RepoListEntry from './RepoListEntry.jsx';
 
 const RepoList = (props) => (
   <div>
-    <h4> Top 25 Repos by Forks </h4>
+    <h4> Top 25 Repos of Users in the Database by Number of Forks </h4>
     <table>
       <tbody>
         {props.repos.map((repo, index) => {
